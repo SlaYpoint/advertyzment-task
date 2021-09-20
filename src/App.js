d96f2f750714264a756d7073f1bb02a7732b53bf
Adithya <66507607+SlaYpoint@users.noreply.github.com>
2021-09-20 14:24:57 +0530

    added data request util

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./styles.css";
 import { Navbar, UserCard } from "./components";
-import { getUsers } from "./utils";
+import { getUsers } from "./utils/net-requests";
+import Loader from "react-loader-spinner";
 
 export default function App() {
   const [loading, setLoading] = useState(false);
+  const [userData, setUserData] = useState([]);
+
+  const buttonHandler = async () => {
+    setLoading(true);
+    try {
+      const data = await getUsers();
+      setUserData(data);
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="App">
-      <Navbar />
-      <UserCard />
+      <Navbar buttonHandler={buttonHandler} />
+      <main>
+        {loading ? (
+          <Loader type="Oval" color="#000" height={100} width={100} />
+        ) : userData.length === 0 ? (
+          <h4>Fetch Users To Display Profiles</h4>
+        ) : (
+          <div className="grid-column-layout">
+            {userData.map((user) => {
+              return <UserCard key={user.id} user={user} />;
+            })}
+          </div>
+        )}
+      </main>
     </div>
   );
 }
--- src/utils/net-requests.js
+++ src/utils/net-requests.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import { API_URL } from "./constants";
 
 export const getUsers = async () => {
-  const { data, status } = await axios.get(API_URL);
+  const {
+    data: { data },
+    status
+  } = await axios.get(API_URL);
 
   if (status === 200 || status === 201) {
     return data;
