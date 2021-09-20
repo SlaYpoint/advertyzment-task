a475d181fda32447a7b0e46a9b18cf149f88c5e7
Adithya <66507607+SlaYpoint@users.noreply.github.com>
2021-09-20 14:25:53 +0530

    added props

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-export const Navbar = () => {
+export const Navbar = ({ buttonHandler }) => {
   return (
     <>
       <nav className="nav">
         <span className="nav-brand-name">brand</span>
         <ul className="nav-link-section">
           <li className="nav-link">
-            <button className="btn btn-primary">Get Users</button>
+            <button className="btn btn-primary" onClick={() => buttonHandler()}>
+              Get Users
+            </button>
           </li>
         </ul>
       </nav>
--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import { FaPlus } from "react-icons/fa";
 
-export const UserCard = () => {
+export const UserCard = ({ user }) => {
   return (
     <div className="card-vertical default-container flex-items shadow-box">
       <div className="img-container">
         <img
           loading="lazy"
           className="img-res card-img product-card-img"
-          src=""
-          alt="user-name"
+          src={user.avatar}
+          alt={user.last_name}
         />
       </div>
       <div className="text-container">
         <div className="text-container-title">
-          <h6>User Name</h6>
+          <h4>{`${user.first_name} ${user.last_name}`}</h4>
         </div>
         <div className="text-container-desc">
-          <p>User email</p>
+          <p>{user.email}</p>
         </div>
         <div className="cta-container">
           <button className="btn btn-icon btn-primary flex-items">
