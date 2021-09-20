cf63c92500dd438837135acdaaaba0377d500d21
Adithya <66507607+SlaYpoint@users.noreply.github.com>
2021-09-20 12:03:49 +0530

    added util for requests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import React, { useState, useEffect } from "react";
 import "./styles.css";
 import { Navbar, UserCard } from "./components";
+import { getUsers } from "./utils";
 
 export default function App() {
+  const [loading, setLoading] = useState(false);
+
   return (
     <div className="App">
       <Navbar />
---
+++ src/utils/net-requests.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { API_URL } from "./constants";
+
+export const getUsers = async () => {
+  const { data, status } = await axios.get(API_URL);
+
+  if (status === 200 || status === 201) {
+    return data;
+  } else {
+    throw new Error("Failed to get users");
+  }
+};
